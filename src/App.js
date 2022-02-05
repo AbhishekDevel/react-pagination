bb0e095cb69927c8480cafe0f3d53247c25f4fc2
AbhishekDevel <abhishek947122@gmail.com>
2022-02-05 14:30:59 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Posts from './Posts';
 import './style.css';
 
 export default function App() {
-  const [posts, setPosts] = useState(null);
+  const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(5);
   useEffect(() => {
     async function getData() {
       const res = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -13,10 +15,14 @@ export default function App() {
     }
     getData();
   }, []);
+  console.log(posts);
+  const indexofLastPost = currentPage * postsPerPage;
+  const indexofFirstPost = indexofLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexofFirstPost, indexofLastPost);
   return (
     <div className="container mt-5">
       <h1 className="text-primary mb-3">My Blog</h1>
-      <Posts posts={posts} loading={loading} />
+      <Posts posts={currentPosts} loading={loading} />
     </div>
   );
 }
