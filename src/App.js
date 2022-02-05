0534c5ba912d3cc05d64e06c1308257f1928025a
AbhishekDevel <abhishek947122@gmail.com>
2022-02-05 13:59:11 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import Posts from './Posts';
 import './style.css';
 
 export default function App() {
-  return(
+  const [posts, setPosts] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data = await res.json();
+      setPosts(data);
+    }
+    getData();
+  }, []);
+  return (
     <div className="container mt-5">
       <h1 className="text-primary mb-3">My Blog</h1>
-      <Posts />
+      <Posts posts={posts} loading={loading} />
     </div>
   );
 }
--- src/Posts.js
+++ src/Posts.js
@@ -1,25 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
 
-export default function Posts() {
-  const [posts, setPosts] = useState(null);
-  useEffect(() => {
-    async function getData() {
-      const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-      const data = await res.json();
-      setPosts(data);
-    }
-    getData();
-  }, []);
-  return (
-    <>
-    <ul className="list-group mb-4">
-      {posts &&
-        posts.map((post) => {
+const Posts = ({posts, loading}) => {
+  if(loading){
+    return <h2>Loading</h2>
+  }
+  return(
+    <div>
+      <ul className="list-group mb-4">
+        {posts && posts.map((post) => {
           return(
-            <li key={post.id} className="list-group-item">{post.title}</li>
-          );
+            <li className="list-group-item" key={post.id}>{post.title}</li>
+          )
         })}
-    </ul>
-    </>
+      </ul>
+    </div>
   );
-}
+};
+
+export default Posts;
